2c398182976df7dd3416a065ebe848f3cc3cd10a
Ralf Hergert <ralf.hergert@haufe-lexware.com>
2018-01-31 15:56:30 +0100

    removed unsued classes

--- src/main/java/de/ralfhergert/telemetry/graph/GraphListener.java
+++
@@ -1,9 +0,0 @@
-package de.ralfhergert.telemetry.graph;
-
-/**
- * Interface for receiving graph changes.
- */
-public interface GraphListener<Key extends Number, Value extends Number> {
-
-	void addedGraphValue(Graph<Key,Value> graph, GraphValue<Key,Value> graphValue, boolean keyDimensionChanged, boolean valueDimensionChanged);
-}
