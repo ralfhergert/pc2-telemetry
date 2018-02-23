f9869a0f6a998cc492fcade8e5d6b257c19f0d5a
Ralf Hergert <ralf.hergert@gmx.de>
2018-02-23 13:18:37 +0100

    added more tests for GraphCanvas

--- src/test/java/de/ralfhergert/telemetry/gui/GraphCanvasTest.java
+++ src/test/java/de/ralfhergert/telemetry/gui/GraphCanvasTest.java
@@ -3,7 +3,10 @@ package de.ralfhergert.telemetry.gui;
 import de.ralfhergert.telemetry.graph.LineGraph;
 import de.ralfhergert.telemetry.repository.IndexedRepository;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.awt.Color;
 
 /**
  * This test ensures the {@link GraphCanvas} is working correctly.
@@ -14,6 +17,12 @@ public class GraphCanvasTest {
 	public void testNullAsGraphIsRejected() {
 		new GraphCanvas<>().addGraph(null);
 		Assert.fail("GraphCanvas should not accept null as graph");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGraphCanvasRejectsRemovingNullGraph() {
+		new GraphCanvas<>().removeGraph(null);
+		Assert.fail("GraphCanvas should not reject removing null graph");
 	}
 
 	@Test
@@ -37,5 +46,53 @@ public class GraphCanvasTest {
 		canvas.removeGraph(graph);
 		Assert.assertEquals("number of graphs in the canvas", 0, canvas.getGraphs().size());
 		Assert.assertEquals("number of listeners on the graph", 0, graph.getListeners().size());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testSettingNullAsZeroLineColorIsRejected() {
+		new GraphCanvas<>().setZeroLineColor(null);
+		Assert.fail("GraphCanvas should not allow null to be set as zero-Line-color");
+	}
+
+	@Test
+	public void testSettingNewZeroLineColorTriggersRepaint() {
+		final CountingGraphCanvas canvas = new CountingGraphCanvas();
+		// set the color white.
+		canvas.setZeroLineColor(Color.WHITE);
+		canvas.clear();
+		Assert.assertEquals("repaint should not have been triggered", 0, canvas.getRepaintsTriggered());
+		canvas.setZeroLineColor(Color.BLACK);
+		Assert.assertEquals("repaint should have been triggered once", 1, canvas.getRepaintsTriggered());
+	}
+
+	@Test
+	public void testSettingTheSameZeroLineColorDoesNotTriggerRepaint() {
+		final CountingGraphCanvas canvas = new CountingGraphCanvas();
+		// set the color white.
+		canvas.setZeroLineColor(Color.WHITE);
+		canvas.clear();
+		Assert.assertEquals("repaint should not have been triggered", 0, canvas.getRepaintsTriggered());
+		canvas.setZeroLineColor(Color.WHITE);
+		Assert.assertEquals("repaint should not have been triggered", 0, canvas.getRepaintsTriggered());
+	}
+
+	@Ignore
+	public class CountingGraphCanvas extends GraphCanvas {
+
+		private int repaintsTriggered = 0;
+
+		@Override
+		public void repaint(long tm, int x, int y, int width, int height) {
+			repaintsTriggered++;
+			super.repaint(tm, x, y, width, height);
+		}
+
+		public int getRepaintsTriggered() {
+			return repaintsTriggered;
+		}
+
+		public void clear() {
+			repaintsTriggered = 0;
+		}
 	}
 }
