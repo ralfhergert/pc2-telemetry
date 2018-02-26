6cb58bbcf80da0eb3f3a81a248f010541d56feba
Ralf Hergert <ralf.hergert@gmx.de>
2018-02-26 09:48:31 +0100

    added more tests for the LookupRepository

--- src/test/java/de/ralfhergert/telemetry/repository/LookupRepositoryTest.java
+++ src/test/java/de/ralfhergert/telemetry/repository/LookupRepositoryTest.java
@@ -41,4 +41,111 @@ public class LookupRepositoryTest {
 		repository.removeItem(42);
 		Assert.assertEquals("number of items in repository", 0, repository.getItemStream().count());
 	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddingNullAsListenerIsRejected() {
+		new LookupRepository<>(String::valueOf).addListener(null);
+	}
+
+	@Test
+	public void testAddingListener() {
+		StoringRepositoryListener<Integer> listener = new StoringRepositoryListener<>();
+		new LookupRepository<String,Integer>(String::valueOf)
+			.addListener(listener)
+			.addItem(3);
+		Assert.assertEquals("number of add event received", 1, listener.getAddedEvents().size());
+		Assert.assertEquals("object in add event", Integer.valueOf(3), listener.getAddedEvents().get(0).getItem());
+	}
+
+	@Test
+	public void testAddingSameListenerTwice() {
+		StoringRepositoryListener<Integer> listener = new StoringRepositoryListener<>();
+		new LookupRepository<String,Integer>(String::valueOf)
+			.addListener(listener)
+			.addListener(listener) // try to add the same listener a second time
+			.addItem(3);
+		Assert.assertEquals("number of add event received", 1, listener.getAddedEvents().size());
+		Assert.assertEquals("object in add event", Integer.valueOf(3), listener.getAddedEvents().get(0).getItem());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testRemovingNullAsListenerIsRejected() {
+		new LookupRepository<>(String::valueOf).removeListener(null);
+	}
+
+	@Test
+	public void testRemovedListenerNoLongerReceivesEvents() {
+		StoringRepositoryListener<Integer> listener = new StoringRepositoryListener<>();
+		new LookupRepository<String,Integer>(String::valueOf)
+			.addListener(listener)
+			.removeListener(listener)
+			.addItem(3);
+		Assert.assertEquals("number of add event received", 0, listener.getAddedEvents().size());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddingNullAsItemIsRejected() {
+		new LookupRepository<String,Integer>(String::valueOf).addItem(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testRemovingNullAsItemIsRejected() {
+		new LookupRepository<String,Integer>(String::valueOf).removeItem(null);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testRemovingItemWithNullAsKeyIsRejected() {
+		new LookupRepository<String,Integer>(String::valueOf).removeItemByKey(null);
+	}
+
+	@Test
+	public void testAddingSameItemASecondTimeIsIgnored() {
+		StoringRepositoryListener<Integer> listener = new StoringRepositoryListener<>();
+		new LookupRepository<String,Integer>(String::valueOf)
+			.addListener(listener)
+			.addItem(3)
+			.addItem(3); // try to add the same item a second time
+		Assert.assertEquals("number of add event received", 1, listener.getAddedEvents().size());
+		Assert.assertEquals("object in add event", Integer.valueOf(3), listener.getAddedEvents().get(0).getItem());
+	}
+
+	@Test
+	public void testRemovingItemFiresEvent() {
+		StoringRepositoryListener<Integer> listener = new StoringRepositoryListener<>();
+		new LookupRepository<String,Integer>(String::valueOf)
+			.addListener(listener)
+			.addItem(3)
+			.removeItem(3);
+		Assert.assertEquals("number of remove event received", 1, listener.getRemovedEvents().size());
+		Assert.assertEquals("object in remove event", Integer.valueOf(3), listener.getRemovedEvents().get(0).getItem());
+	}
+
+	@Test
+	public void testRemovingNonExistentItemNotFiresEvent() {
+		StoringRepositoryListener<Integer> listener = new StoringRepositoryListener<>();
+		new LookupRepository<String,Integer>(String::valueOf)
+			.addListener(listener)
+			.removeItem(3);
+		Assert.assertEquals("number of remove event received", 0, listener.getRemovedEvents().size());
+	}
+
+	@Test
+	public void testRemovingItemByKeyFiresEvent() {
+		StoringRepositoryListener<Integer> listener = new StoringRepositoryListener<>();
+		new LookupRepository<String,Integer>(String::valueOf)
+			.addListener(listener)
+			.addItem(3)
+			.removeItemByKey("3");
+		Assert.assertEquals("number of remove event received", 1, listener.getRemovedEvents().size());
+		Assert.assertEquals("object in remove event", Integer.valueOf(3), listener.getRemovedEvents().get(0).getItem());
+	}
+
+	@Test
+	public void testRemovingNonExistentItemByKeyNotFiresEvent() {
+		StoringRepositoryListener<Integer> listener = new StoringRepositoryListener<>();
+		new LookupRepository<String,Integer>(String::valueOf)
+			.addListener(listener)
+			.removeItemByKey("3");
+		Assert.assertEquals("number of remove event received", 0, listener.getRemovedEvents().size());
+	}
 }
