4076f238f36252610be6b21a7d892ff72b50513c
Ralf Hergert <ralf.hergert@gmx.de>
2018-02-28 12:10:01 +0100

    made constructor of utility class private

--- src/main/java/de/ralfhergert/telemetry/util/ImageBuilder.java
+++ src/main/java/de/ralfhergert/telemetry/util/ImageBuilder.java
@@ -5,6 +5,8 @@ import java.awt.image.BufferedImage;
 
 public class ImageBuilder {
 
+	private ImageBuilder() { /* no instantiation necessary */ }
+
 	public static Image createSingleColoredImage(int width, int height, Color color) {
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g = (Graphics2D)image.getGraphics();
